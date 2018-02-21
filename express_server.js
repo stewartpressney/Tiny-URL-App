346f57d669b9e3753b485350a61ea984d08af9b3
stewartpressney <stewartpressney@gmail.com>
2018-02-20 20:13:18 -0800

    second commit added redirect and random math function

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // defalt port
-
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 
@@ -10,15 +10,47 @@ const urlDatabase = {
   "2134" : "http://www.google.com"
 };
 
+//generate random number for URL ID
+function generateRandomString() {
+  let text = "";
+  let possible = "0123456789";
 
+  for (let i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+app.get("/u/:shortURL", (request, response) => {
+  let longURL = urlDatabase[request.params.shortURL];
+  response.redirect(longURL);
+});
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
+});
+
+app.post("/urls", (request, response) => {
+  console.log(request.body); //view post params
+  response.send("OK");       //respond with okay
+});
 
 app.get("/urls", (request, response) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase
+  };
   response.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (request, response) => {
-  let templateVars = {shortURL: request.params.id, urls: urlDatabase, port: PORT};
+  let templateVars = {
+    shortURL: request.params.id,
+    urls: urlDatabase,
+    port: PORT
+  };
   response.render("urls_show", templateVars);
 });
 
