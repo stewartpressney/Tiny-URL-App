36d9430b6e4cbc211bb1fe23e33b0d3985eecf99
stewartpressney <stewartpressney@gmail.com>
2018-02-20 22:37:06 -0800

    Tiny app takes URL's redirects to a undefined page and has the homepage updated with a 4 digit code - end of tiny App Day 1 - 10:35pm

--- express_server.js
+++ express_server.js
@@ -12,13 +12,13 @@ const urlDatabase = {
 
 //generate random number for URL ID
 function generateRandomString() {
-  let text = "";
+  let tinyURL = "";
   let possible = "0123456789";
 
-  for (let i = 0; i < 5; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  for (let i = 0; i < 4; i++)
+    tinyURL += possible.charAt(Math.floor(Math.random() * possible.length));
 
-  return text;
+  return tinyURL;
 }
 
 app.get("/u/:shortURL", (request, response) => {
@@ -26,16 +26,18 @@ app.get("/u/:shortURL", (request, response) => {
   response.redirect(longURL);
 });
 
-
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/urls/new", (request, response) => {
   response.render("urls_new");
 });
 
+
+//take in string and post it to the URLdatabase object
 app.post("/urls", (request, response) => {
-  console.log(request.body); //view post params
-  response.send("OK");       //respond with okay
+  urlDatabase[generateRandomString()] = request;
+  console.log(request.body);
+  response.redirect("urls/:id");
 });
 
 app.get("/urls", (request, response) => {
