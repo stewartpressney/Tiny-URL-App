fbe431bb2129469f5d3e4ad1d30ead065c6f827b
stewartpressney <stewartpressney@gmail.com>
2018-02-22 18:44:53 -0800

    login/logout WORKING sorta

--- express_server.js
+++ express_server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // defalt port
 const bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
+const flash = require('connect-flash');
 
 app.set("view engine", "ejs");
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(flash());
 
 const urlDatabase = {
   "1234" : "http://www.lighthouselabs.ca",
@@ -52,7 +54,7 @@ app.get("/u/:shortURL", (request, response) => {
 //Show New URL Page
 app.get("/urls/new", (request, response) => {
   let templateVars = {
-    email: response.cookie["user_emailCookie"],
+    email: request.cookies["user_emailCookie"],
     id: response.cookie["user_IDCookie"]
   };
   response.render("urls_new", templateVars);
@@ -81,32 +83,65 @@ app.get("/register", (request, response) => {
 
 //post registration info
 app.post("/register", (request, response) => {
-  let email = request.body.email
-  let key = generateRandomString()
-  let user = key
-    key.id = key
-    key.email = request.body.email
-    key.password = request.body.password
-  users[key] = user
-  response.cookie('user_emailCookie', email)
-  response.cookie('user_IDCookie', key)
-  response.redirect("/urls");
+  let key = generateRandomString();
+  let email = request.body.email;
+  // let password = request.body.password
+  let newUser = {
+    id: key,
+    email: request.body.email,
+    password: request.body.password
+  };
+  //let urls = urlDatabase;
+  users[key] = newUser;
+  for (user in users){
+    if (user.email === request.body.email){
+      response.send('You Are Already Registered.');
+      response.status(400);
+
+    } if (request.body.email === "" || request.body.password === "") {
+      response.send('You Shall Not Pass -____-');
+      response.status(400);
+
+    } else {
+      //users[key] = newUser;
+      //response.cookie('user_emailCookie', email);
+      response.cookie('user_IDCookie', key);
+      //console.log(users, email, key, user, newUser)
+      response.redirect("/urls");
+   }
+   console.log(users)
+  };
 });
 
 
 //login
 app.post("/login", (request, response) =>{
   let email = request.body.email
-  response.cookie('user_emailCookie', email)
-  console.log(request)
-  response.redirect("/urls");
+  let isUser;
+  for (let user in users){
+    const verifyUser = users[user]
+    if (verifyUser.email === request.body.email){
+      isUser = verifyUser
+      break
+    }
+  }
+  if (isUser){
+      if (isUser.password === request.body.password){
+        response.cookie('user_IDCookie', isUser.id)
+        response.redirect("/urls");
+      } else {
+        response.status(401).send('There was a problem with your login, please try again.');
+      }
+  } else {
+    response.status(401).send('There was a problem with your login, please try again.');
+  }
 });
 
 
 //logout
 app.post("/logout", (request, response) =>{
     let templateVars = {
-    email: response.cookie["user_emailCookie"],
+    email: request.cookies["user_emailCookie"],
     id: response.cookie["user_IDCookie"]
   };
   response.clearCookie('user_emailCookie');
@@ -128,12 +163,17 @@ app.post("/urls", (request, response) => {
 app.get("/urls", (request, response) => {
   let templateVars = {
     //username: request.cookies["usernameCookie"],
+
     urls: urlDatabase,
-    email: response.cookie["user_emailCookie"],
-    id: response.cookie["user_IDCookie"]
+    user: users[request.cookies.user_IDCookie]
+    //id: response.cookie["user_IDCookie"]
   };
+  //console.log(users)
+  console.log(templateVars)
+  console.log('Cookies: ', request.cookies)
+  console.log('Cookies: ', request.cookie)
   console.log(users)
-  response.render('urls_index', templateVars);
+  response.render("urls_index", templateVars);
 });
 
 
